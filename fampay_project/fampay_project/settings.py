29ef9091b44f958bb01b5a32e8656f67ceeed933
Satyam Raj <44699922+Satyam-Raj@users.noreply.github.com>
2021-12-19 22:46:52 +0530

    settings update for youtube key

--- fampay_project/fampay_project/settings.py
+++ fampay_project/fampay_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+YOUTUBE_DATA_API_KEY = 'AIzaSyCpKShk45vQPc668hZIaJ130HiBFSCQpuA'
